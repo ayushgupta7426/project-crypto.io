ac1622af217eb7b3fa50ee28b4df01367cc66f1b
ayush <9956624639a@gmail.com>
2022-07-24 02:58:59 +0530

    new commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const { middleware } = require('./middleware');
 const axios=require('axios');
-const port=process.env.port ||3000;
+const port=process.env.PORT ||3000;
 const dotenv=require('dotenv');
 dotenv.config();
 
